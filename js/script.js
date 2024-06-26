6758918faf39b1967e4698b502da7e8263aabc16
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-26 14:50:29 +0200

    Build:add basic project files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const { createApp } = Vue ;
+
+createApp( {   
+    data() {   
+        return {
+        }
+    },
+    methods: {
+       
+    },
+    mounted() {
+        console.log("the component is now mounted");
+    }
+}).mount('#app')  
