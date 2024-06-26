bad191a95315dbd9b22c43f15b972ca6030a7601
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-26 16:32:44 +0200

    Feat: print task and add class done if task is done

--- js/script.js
+++ js/script.js
@@ -3,6 +3,21 @@ const { createApp } = Vue ;
 createApp( {   
     data() {   
         return {
+            logoImage:"img/logo.jpg",
+            tasks:[
+                {
+                    text: "fare il bucato",
+                    done: true,
+                },
+                {
+                    text: "fare i compiti",
+                    done: false,
+                },
+                {
+                    text: "fare la spesa",
+                    done: true,
+                },
+            ]
         }
     },
     methods: {
