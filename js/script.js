92610aa13850bc7c883d062e36d0f815527bbe50
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-26 17:24:31 +0200

    Feat: fix addTask method and add input error

--- js/script.js
+++ js/script.js
@@ -4,12 +4,10 @@ createApp( {
     data() {   
         return {
             logoImage:"img/logo.jpg",
-            newTask:[
-                {
+            newTask:{
                     text: "",
                     done: false,
-                }
-            ],
+                },
             inputError: false,
             tasks:[
                 {
@@ -34,14 +32,20 @@ createApp( {
         addTask(){
             //aggiungo task all'inizio
             //condizione di check validità input
+            console.log(this.newTask.text,this.newTask);
             if(this.newTask.text !== '' && this.newTask.text.length >= 5){
-                this.tasks.unshift(this.newTask);
+                /* this.tasks.unshift({
+                    text:this.newTask.text,
+                    done: false,
+                }); */
+                const newStaticTask = {...this.newTask};
+                this.tasks.unshift(newStaticTask);
                 this.inputError = false;
             }else{
                 this.inputError = true;
             }
             //svuoto il valore di new task
-            this.newTask = "";
+            this.newTask.text = "";
             
         },
     },
