10a2c72605c4c989dcbe548ebf590dab0114125b
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-26 16:46:11 +0200

    Feat: add addTask method

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@ createApp( {
     data() {   
         return {
             logoImage:"img/logo.jpg",
+            newTask:[
+                {
+                    text: "",
+                    done: false,
+                }
+            ],
+            inputError: false,
             tasks:[
                 {
                     text: "fare il bucato",
@@ -23,6 +30,19 @@ createApp( {
     methods: {
         deleteTask(i){
             this.tasks.splice(i, 1)
+        },
+        addTask(){
+            //aggiungo task all'inizio
+            //condizione di check validità input
+            if(this.newTask.text !== '' && this.newTask.text.length >= 5){
+                this.tasks.unshift(this.newTask);
+                this.inputError = false;
+            }else{
+                this.inputError = true;
+            }
+            //svuoto il valore di new task
+            this.newTask = "";
+            
         },
     },
     mounted() {
